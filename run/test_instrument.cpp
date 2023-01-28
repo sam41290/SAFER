b337829673a9a707bc8c0200d3215ecac684598e
Rohit Chouhan <rohitchouhan07@gmail.com>
2023-01-28 14:10:52 -0500

    updated

--- run/test_instrument.cpp
+++ run/test_instrument.cpp
@@ -16,11 +16,11 @@ main (int argc, char *args[]) {
   cout << binary_path << endl;
   Binary b (binary_path);
 
-  vector<InstArg> arglst;
-  arglst.push_back(InstArg::EXENAME);
-  arglst.push_back(InstArg::RIP);
-  arglst.push_back(InstArg::INDIRECT_TARGET);
-  b.registerInstrumentation(InstPoint::INDIRECT_CF,"LOG",arglst);
+  //vector<InstArg> arglst;
+  //arglst.push_back(InstArg::EXENAME);
+  //arglst.push_back(InstArg::RIP);
+  //arglst.push_back(InstArg::INDIRECT_TARGET);
+  //b.registerInstrumentation(InstPoint::INDIRECT_CF,"LOG",arglst);
   //
 
   vector<InstArg> arglst2;
