7ac8b4aa9648fb27a37f8151dc63705f9c1c6b49
sam41290 <soumya41290@gmail.com>
2023-05-14 23:31:04 -0400

    updated

--- src/cfg/BasicBlock.cpp
+++ src/cfg/BasicBlock.cpp
@@ -375,7 +375,7 @@ BasicBlock::instrument() {
         if(ins->asmIns().find("ret") != string::npos) {
           ins->registerInstrumentation(p.first,p.second,allargs[p.second]);
         }
-        else if(ins->isCall()) {
+        else if(ins->isCall() && ins->asmIns().find("syscall") == string::npos) {
           ins->registerInstrumentation(p.first,p.second,allargs[p.second]);
           ins->mnemonic("jmp");
           ins->asmIns(ins->mnemonic() + " " + ins->op1());
